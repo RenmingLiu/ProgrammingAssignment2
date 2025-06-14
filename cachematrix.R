a0df36ed39d8d5fe014d7115a1e9a74101a0cb11
刘人铭 <liurenming@s-MacBook-Pro.local>
2025-06-15 00:27:14 +0800

    “complete”

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+# How to Use This Code:
 
-## Write a short comment describing this function
+# 1. First, create a square, invertible matrix.
+#    For example, a 3x3 matrix with random numbers.
+random_matrix <- matrix(rnorm(9), nrow = 3)
 
-makeCacheMatrix <- function(x = matrix()) {
+# 2. Next, use makeCacheMatrix() to create a special object
+#    that encapsulates the matrix and its cache.
+cached_object <- makeCacheMatrix(random_matrix)
 
+# 3. Finally, pass this special object to cacheSolve().
+#    The first time, it will compute the inverse.
+#    Subsequent times, it will retrieve the result from the cache.
+cacheSolve(cached_object)
+
+
+makeCacheMatrix <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setcache <- function(solve) m <<- solve
+    getcache <- function() m
+    list(set = set, get = get,
+         setcache = setcache,
+         getcache = getcache)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getcache()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setcache(m)
+    m
 }
